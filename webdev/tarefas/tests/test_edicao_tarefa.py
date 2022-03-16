e2bdadf26a13927787bc728ba687ae907359885e
Robson Leal <devleal@hotmail.com>
2022-03-16 16:09:45 -0300

    Resolved issue #6

--- webdev/tarefas/tests/test_edicao_tarefa.py
+++ webdev/tarefas/tests/test_edicao_tarefa.py
@@ -20,3 +20,18 @@ def test_status_code(response_with_pending_task):
 
 def test_completed_task(response_with_pending_task):
     assert Tarefa.objects.first().concluida
+
+@pytest.fixture
+def completed_task(db):
+    return Tarefa.objects.create(nome='Tarefa 01', concluida=True)
+
+@pytest.fixture
+def response_with_completed_task(client, completed_task):
+    resp = client.post(
+        reverse('tarefas:detalhe', kwargs={'tarefa_id':completed_task.id}),
+        data={'nome':f'{completed_task.nome}-edited'} 
+    )
+    return resp
+
+def test_pending_task(response_with_completed_task):
+    assert not Tarefa.objects.first().concluida
